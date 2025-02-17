1340b141cb397c0bc2a290c24ce9ac3152497952
anu-bazar <anubazarragcaa@gmail.com>
2025-02-17 11:41:27 +0100

    fibonacci

--- problem2.py
+++ problem2.py
@@ -7,4 +7,16 @@ By starting with 1 and 2, the first terms will be:
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
 find the sum of the even-valued terms.
 """
-
+fibonacci = [1,1]
+while (fibonacci[-1]) < 4000000:
+    if (len(fibonacci)) < 2:
+        fibonacci.append(fibonacci[0])
+    elif (len(fibonacci)) >= 2:
+        next_number = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_number)         
+print(sum(fibonacci))
+even_fibonacci = []
+for x in fibonacci:
+    if x % 2 == 0:
+        even_fibonacci.append(x)
+print(sum(even_fibonacci))
